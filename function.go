764c5f45cc9aaf5509a621087120330fb442e075
Sam <sam@developersam.com>
2021-04-14 10:41:10 -0400

    Handle CORS

--- function.go
+++ function.go
@@ -10,6 +10,18 @@ import (
 
 // HandleTenAIMoveRequest is an HTTP Cloud Function to handle a client request for an AI move.
 func HandleTenAIMoveRequest(writer http.ResponseWriter, request *http.Request) {
+	// Set CORS headers for the preflight request
+	if request.Method == http.MethodOptions {
+		writer.Header().Set("Access-Control-Allow-Origin", "*")
+		writer.Header().Set("Access-Control-Allow-Methods", "POST")
+		writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		writer.Header().Set("Access-Control-Max-Age", "3600")
+		writer.WriteHeader(http.StatusNoContent)
+		return
+	}
+	// Set CORS headers for the main request.
+	writer.Header().Set("Access-Control-Allow-Origin", "*")
+
 	var clientBoard ten.BoardData
 	if err := json.NewDecoder(request.Body).Decode(&clientBoard); err != nil {
 		writer.WriteHeader(http.StatusBadRequest)
